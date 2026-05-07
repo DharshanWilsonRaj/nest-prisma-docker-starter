bca0fd101ddf4d2fcf9c00a8e6da02d2e85495f7
Dharshan <dharshan@selvisoftware.com>
2026-05-07 13:37:30 +0530

    migratation

--- server/prisma/migrations/20260507075509_add_post_comment/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "post_comment" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- server/prisma/migrations/20260507075607_dev/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `post_comment` on the `Post` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Post" DROP COLUMN "post_comment";
